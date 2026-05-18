f3f79ebaed7b62991c99a75ee383e0713d7bd0f0
avetisyanmarine <marineavet2006@gmail.com>
2026-05-19 02:33:08 +0400

    poiugfd

--- src/components/third-page/styled.jsx
+++ src/components/third-page/styled.jsx
@@ -56,7 +56,7 @@ const ThirdPagePartCalendar = styled.div`
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
-      background-image: url("/heart.webp");
+      background-image: url("/heart.png");
       background-size: cover;
       z-index: -1;
     }
